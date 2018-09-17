dfecdd28bfd636699ddd2ab9debe984a65685cfd
nuronics <42369707+nuronics@users.noreply.github.com>
2018-09-17 12:33:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import requests
 import json
 import os
 import googlemaps
-import dialogflow_v2
-#from geolocation.main import GoogleMaps
-#from geolocation.distance_matrix.client import DistanceMatrixApiClient
-#from googlemaps import exceptions
+#import dialogflow_v2
+from geolocation.main import GoogleMaps
+from geolocation.distance_matrix.client import DistanceMatrixApiClient
+from googlemaps import exceptions
+uloc =''
 
 app = flask.Flask(__name__)
 
@@ -29,14 +30,20 @@ def processRequest(req):
     if result.get("action")== "AskLocation.AskLocation-yes":
         loc=getLocation()
     parameters = result.get("parameters")
-    u_loc = str(parameters.get("landmark"))
+
+    landm = str(parameters.get("landmark"))
+    if u_loc == landmark:
+        continue
+    else:
+        u_loc = geolocationn(landm)
+
     u_type = str(parameters.get("type"))
     u_cuisine = str(parameters.get("cuisines"))
     u_Collections = str(parameters.get("Collections"))
     #url to fetch the details of the client location
     location_url=url+'locations?query='+u_loc+apikey
     json_data = requests.get(location_url).json()
-    
+
     entity_id=json_data.get('location_suggestions')[0].get('entity_id')
     entity_type=json_data.get('location_suggestions')[0].get('entity_type')
     city_id=json_data.get('location_suggestions')[0].get('city_id')
@@ -53,12 +60,12 @@ def processRequest(req):
         namedict.append(json_data.get('restaurants')[x].get('restaurant').get('name'))
         urldict.append(json_data.get('restaurants')[x].get('restaurant').get('order_url'))
         resultstr = makeresult(resultstr,json_data.get('restaurants')[x].get('restaurant').get('name'))
-    
+
     #speech=str(namedict)+str(urldict)
     dict = {}
     for x in range(len(json_data.get('restaurants'))):
         dict[json_data.get('restaurants')[x].get('restaurant').get('name')] = json_data.get('restaurants')[x].get('restaurant').get('order_url')
-    
+
     speech="These are the available Best restaurants in your area, which serve the "+u_cuisine+"\n"+resultstr+"\n Choose one restaurant of your choice."
     print(speech)
     return {
@@ -67,12 +74,46 @@ def processRequest(req):
         "source":"Zomato top restaurants"
     }
 
+def geolocationn(u_loc):
+    if u_loc == 'none':
+        address = "chaipani jubliee hills checkpost Hyderabad"
+    else:
+        address = u_loc
+        print (address)
+    google_maps = GoogleMaps(api_key='AIzaSyDpUu4Ks4VMyW02wV_rp2sWS2jFlzAM09M')
+    if googlemaps != None:
+
+        location = google_maps.search(location=address)
+
+    #    print(location.all())
+
+        my_location = location.first()
+
+        print(my_location.city)
+        print(my_location.route)
+        print(my_location.street_number)
+        print(my_location.postal_code)
+
+        for administrative_area in my_location.administrative_area:
+            print("%s: %s" % (administrative_area.area_type, administrative_area.name))
+
+        print(my_location.country)
+        print(my_location.country_shortcut)
+
+        print(my_location.formatted_address)
+
+        print(my_location.lat)
+        print(my_location.lng)
+
+    return my_location.formatted_address
+
+
 def makeresult(resultstr,appendstr):
-    
+
     return resultstr+"\n"+str(appendstr)
 
 '''def fetchcuisines(u_loc):
-    cusineurl = url+'cusines?'+u_loc+apikey   
+    cusineurl = url+'cusines?'+u_loc+apikey
     json_data = requests.get(cusineurl).json()
     cuisines = json_data.get('cusines')'''
 
