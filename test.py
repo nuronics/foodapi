068bc0793b8ed549babcbacd765e5bd905db859d
nuronics <42369707+nuronics@users.noreply.github.com>
2018-09-27 14:47:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -113,4 +113,4 @@ def makeresult(resultstr,appendstr):
 if __name__ == '__main__':
     port = int(os.getenv('PORT',5000))
     print("Starting app %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
